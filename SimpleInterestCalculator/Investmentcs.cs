d453ac8058119f34c16039bc40870586bf9b7895
Munirudeen Akanbi <adsdesigntech@gmail.com>
2024-04-26 10:48:41 +0100

    Simple interest Calculator Added to project

---
+++ SimpleInterestCalculator/Investmentcs.cs
@@ -0,0 +1,43 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Channels;
+using System.Threading.Tasks;
+
+namespace SimpleInterestCalculator
+{
+    public class Investments
+    {
+        //Properties
+        public double Principal {  get; set; }
+        public double Rate { get; set; }
+        public int Time {  get; set; }
+
+        //Method for Calculating Simple Interest
+        public double CalculateSimpleInterest()
+        {
+            return (Principal * Rate * Time)/100;
+        }
+
+        //Method for Calculating Final Amount
+
+        public double CalculateFinalAmount()
+        {
+            return Principal + CalculateSimpleInterest(); 
+        }
+
+        //Method for Calculating Interest Rate
+        public double CalculateIntrestRate(double principal)
+        {
+            return (CalculateSimpleInterest() * 100) / (principal * Time);
+        }
+        //Method for calculating duration
+        public int CalculateInvestmentDuration( int principal,double rate)
+        {
+            return (int)(CalculateSimpleInterest() * 100 / (principal * rate));
+            
+        }
+
+    }
+}
---
+++ SimpleInterestCalculator/Program.cs
@@ -0,0 +1,65 @@
+﻿using System;
+
+namespace SimpleInterestCalculator
+{
+    public class Program
+    {
+        static void Main(string[] args)
+        {
+            //Create an Instace of the investment class
+            Investments FirstInvestment = new Investments();
+
+            //Prompt the user to enter the principal amount,interest rate and investment duration(in years)
+            Console.WriteLine("Welcome to your Simple Interest Calculator");
+            Console.WriteLine("-----------------------------------------");
+            Console.WriteLine("Pls enter your name");
+            string name = Console.ReadLine();
+
+
+
+            Console.WriteLine("Pls enter your principal amount");
+            double userPrincipalAmount = double.Parse(Console.ReadLine());
+
+            Console.WriteLine("Pls enter your interest rate");
+            double userInterestRate = double.Parse(Console.ReadLine());
+
+            Console.WriteLine("Pls enter your duration");
+            int userInvestmentDuration = int.Parse(Console.ReadLine());
+
+            //Assigning User Inputs to Field.
+            FirstInvestment.Principal = userPrincipalAmount;
+            FirstInvestment.Rate = userInterestRate;
+            FirstInvestment.Time = userInvestmentDuration;
+
+
+            //Call the respective methods to calculate
+            //the simple interest,
+            //final amount,
+            //interest rate (based on the final amount),
+            //and investment duration (based on the final amount).
+
+            //(a) Calculating Simple Interest
+            double simpleInterestResult=FirstInvestment.CalculateSimpleInterest();
+            //(b) calculating final Amount
+            double finalAmountResult = FirstInvestment.CalculateFinalAmount();
+            //(c) calculating interest rate
+            double interestRateResult = FirstInvestment.CalculateIntrestRate( userPrincipalAmount);
+            //(d) calculating investment duration
+            int investmentDurationResult = FirstInvestment.CalculateInvestmentDuration( (int)userPrincipalAmount, userInterestRate);
+
+            Console.WriteLine($"Your inputed data are Principal :{userPrincipalAmount}.\n Rate: {userInterestRate}.\n Time :{userInvestmentDuration}years");
+            Console.WriteLine("-----------------------------------------------------------------");
+            Console.WriteLine("-----------------------------------------------------------------");
+            Console.WriteLine("Calculating Based on your Data");
+            Console.WriteLine($"Welcome{name}\n Simple Interest: {simpleInterestResult}.\n Final-Amount: {finalAmountResult}.\n Interest Rate: {interestRateResult}.\n Investment-Duration: {investmentDurationResult}");
+
+
+            //int c1 = 1800;
+            //int c2 = 900;
+            //int calc = c1 / c2;
+
+            //Console.WriteLine(calc);
+
+        }
+    }
+}
